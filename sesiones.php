7b9a2f4915d4792dd37608a6799258ec8aced8d6
Ismael-Garcia-Martinez <ismagarmar2005@gmail.com>
2025-02-22 11:22:59 +0100

    Update sesiones.php

--- sesiones.php
+++ sesiones.php
@@ -9,26 +9,26 @@ if (!$conn)
 {
     die ("Conexion fallida: " . mysqli_connect_error());
 }
+
 $nombre = trim(strip_tags($_REQUEST['usuario']));
 $dni = trim(strip_tags($_REQUEST['dni']));
 $pass = trim(strip_tags($_REQUEST['contra']));
 $pass_cifrada = password_hash($pass, PASSWORD_DEFAULT);
-$sql = "SELECT * FROM usuarios where nombre = '$nombre' and dni = '$dni'";
+
+$sql = "SELECT * FROM usuarios where nombre = '$nombre' and dni = '$dni' and password = '$pass_cifrada'";
 $consulta = mysqli_query($conn,$sql) or die ("Fallo en la consulta" . mysqli_error($conn));
-$resultado = mysqli_fetch_array($consulta);
-if (!$resultado){
-    echo "Usuario no encontrado";
-    exit();
-}
-if (password_verify($pass_cifrada,$resultado['password']))
+
+if (mysqli_num_rows($consulta) == 1)
 {
-    $_SESSION["nombre"] = $nombre;
+    $usuario = mysqli_fetch_assoc($consulta);
+
+    $_SESSION['usuario'] = $usuario['nombre'];
+    $_SESSION['dni'] = $usuario['dni']
     header("Location: index-cliente.php");
     exit();
 }
 else
 {
-    echo "Credenciales incorrectas";
+    echo "Credenciales incorrectas"
 }
-mysqli_close($conn);
 ?>
